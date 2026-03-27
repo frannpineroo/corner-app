4fbefd53a8b57d79f5f9f9c0115a668ed9fe24e5
frannpineroo <frannpinero@hotmail.com>
2026-03-27 11:18:40 -0300

    Arreglando el problema de autenticacion

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,50 +1,24 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabase";
 import type { User } from "@supabase/supabase-js";
-import type { Tables } from "../types/supabase";
-
-type Profile = Tables<"profiles">;
 
 export const useAuth = () => {
     const [user, setUser] = useState<User | null>(null);
-    const [profile, setProfile] = useState<Profile | null>(null);
     const [loading, setLoading] = useState(true);
 
-    const fetchProfile = async (userId: string) => {
-        const { data } = await supabase
-            .from("profiles")
-            .select("*")
-            .eq("id", userId)
-            .single();
-        setProfile(data ?? null);
-    }
-
     useEffect(() => {
-        const init = async () => {
-            const { data } = await supabase.auth.getUser();
-            setUser(data.user);
-            if (data.user) await fetchProfile(data.user.id);
+        supabase.auth.getSession().then(({ data }) => {
+            console.log("sesion:", data.session)
+            setUser(data.session?.user ?? null);
             setLoading(false);
-        }
+        });
 
-        init();
-
-        const { data: listener } = supabase.auth.onAuthStateChange(
-            async (_, session) => {
-                setUser(session?.user ?? null)
-                if (session?.user) await fetchProfile(session.user.id);
-                else {
-                    setProfile(null);
-                    setLoading(false);
-                }
-            }
-        )
+        const { data: listener } = supabase.auth.onAuthStateChange((_, session) => {
+            setUser(session?.user ?? null);
+        });
 
         return () => listener.subscription.unsubscribe();
-    }, [])
+    }, []);
 
-    const isAdmin = profile?.rol === "admin";
-    const isProfesor = profile?.rol === "profesor";
-
-    return { user, profile, loading, isAdmin, isProfesor };
-}
+    return { user, loading };
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import { useAuth } from './hooks/useAuth';
 // import AlumnosPage from './pages/AlumnosPage';
 import Login from './pages/Login';
 
+function App() {
+  const { user, loading } = useAuth();
+
+  if (loading) return <p>Cargando...</p>
+  if (!user) return <Login />
+
+  return <p>Bienvenido, estas logueado</p>
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Login />
+    <App />
   </StrictMode>,
 )
