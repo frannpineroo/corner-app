0b1ffb23408497838ab12b21ca84c8bf8331cc84
frannpineroo <frannpinero@hotmail.com>
2026-03-30 19:16:50 -0300

    Arreglando problemas de autenticacion

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import AdminPanel from "./pages/AdminPanel";
 import ProfesorPanel from "./pages/ProfesorPanel";
 
 export default function App() {
-    const { user, loading, isAdmin } = useAuth();
+    const { user, profile, loading, isAdmin } = useAuth();
 
-    if (loading) return <p>Cargando...</p>
+    if (loading) return <p className="min-h-screen bg-gray-900 text-white flex items-center justify-center">Cargando...</p>
     if (!user) return <Login />
+    if (!profile) return <p className="min-h-screen bg-gray-900 text-white flex items-center justify-center">Cargando...</p>
     if (isAdmin) return <AdminPanel />
-
     return <ProfesorPanel />
 }
